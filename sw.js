b488e9085fe426d91558acd4558d1d5ae0811c5d
eubannikov-bit <eu.bannikov@gmail.com>
2026-03-25 15:10:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,21 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
+const CACHE_NAME = "money-app-v1";
 
-self.addEventListener('activate', event => {
+const urlsToCache = [
+  "/money-app/",
+  "/money-app/index.html",
+  "/money-app/icon.png"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', () => {
-  // cache disabled
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
